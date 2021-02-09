9fadd3d5b404ce0322c82943a1567ac9038598c5
bjoernp <54497210+pressAltF4forFree@users.noreply.github.com>
2021-02-09 22:35:39 +0100

    make it better

--- lessCoolBot.js
+++ lessCoolBot.js
@@ -1,23 +1,47 @@
-https = require("https")
-fs = require("fs")
+const https = require("https")
+const fs = require("fs")
+const Twit = require("twit")
+
+funFact()
+setInterval(funFact, 1000*60*60*12)
+function funFact(){
+    https.get('https://uselessfacts.jsph.pl/random.json?language=en', (resp) => {
+    let data = '';
+
+    // A chunk of data has been received.
+    resp.on('data', (chunk) => {
+        data += chunk;
+    });
+
+    // The whole response has been received. Print out the result.
+    resp.on('end', () => {
+        let json = JSON.parse(data).text;
+        console.log(JSON.parse(data).text);
+
+
+        T.post('statuses/update', {status: json} , function(err, data, response) {
+            console.log(data)
+        })
+
+
+    });
+
+    }).on("error", (err) => {
+    console.log("Error: " + err.message);
+    });
+
+
+    console.log("bot running!")
+
+    var T = new Twit({
+        consumer_key:         'CNVWCpptNn3ysseDYmTJLNPC1',
+        consumer_secret:      'VacNgSEWS9olTpqSPFPeau9XyRl3Kti3gmNWyTkyk9gcDKOXhY',
+        access_token:         '1359219804276727814-oX8GBXhZp8YfR0TDA1CZRgS53RmC8X',
+        access_token_secret:  'rtHTupX3ikC1qt7zpVJccXSSGrXI6lQhNejWUPpYMh3tj'
+    });
 
 
 
-https.get('https://uselessfacts.jsph.pl/random.json?language=en', (resp) => {
-  let data = '';
 
-  // A chunk of data has been received.
-  resp.on('data', (chunk) => {
-    data += chunk;
-  });
 
-  // The whole response has been received. Print out the result.
-  resp.on('end', () => {
-    let json = JSON.parse(data).text;
-    console.log(JSON.parse(data).text);
-    fs.writeFile('fact.txt', json , function(err) {})
-  });
-
-}).on("error", (err) => {
-  console.log("Error: " + err.message);
-});
+}
