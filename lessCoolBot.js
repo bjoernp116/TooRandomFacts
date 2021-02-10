2c971b4462e6ed4e9b523667cc312b71b0fe6a54
bjoernp <54497210+pressAltF4forFree@users.noreply.github.com>
2021-02-10 20:54:11 +0100

    worker

--- lessCoolBot.js
+++ lessCoolBot.js
@@ -3,7 +3,7 @@ const fs = require("fs")
 const Twit = require("twit")
 
 funFact()
-setInterval(funFact, 1000*60*60*12)
+setInterval(funFact, 1000*60*60)
 function funFact(){
     https.get('https://uselessfacts.jsph.pl/random.json?language=en', (resp) => {
     let data = '';
